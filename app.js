1259d08fd7a7205b559d23da32d91badc1613608
Kris <christellelocque@hotmail.fr>
2017-12-08 15:47:19 +0100

    premier jet de résolution

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// good luck !
+function generale() {
+		//boucle pour chaque boutton
+		$('.color').each(function() {
+			//sélectionner les boutons un par un avec leur data
+			console.log($(this).data('color'));
+		//Attribuer la data à la classe "color"
+		$(this).css("background-color", $(this).data("color"));
+
+		$(this).click(function() {
+			if ($('#modify-texte').is(':checked')) {
+				$('p').css("color", $(this).data("color"));
+			}
+
+			else {
+				$('.main').css("background-color", $(this).data("color"));
+			}
+
+		});
+	});
+}
+
+generale()
